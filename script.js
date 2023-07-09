eef8546697e1b31f4d97d64dbaffbf12561faae2
Kanikagarwal <93864492+Kanikagarwal@users.noreply.github.com>
2023-07-09 10:31:40 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+ setInterval(() => {
+     let sound = new Audio("tick.mp3");
+     sound.play();
+    let date = new Date();
+    let htime = date.getHours();
+    let mtime = date.getMinutes();
+    let stime = date.getSeconds();
+let hrotation = 30*htime + mtime/2;
+let mrotation = 6*mtime + stime/10;
+let srotation = 6*stime;
+let hour = document.getElementById("hour");
+let minute = document.getElementById("minute");
+let second = document.getElementById("second");
+hour.style.transform = `rotate(${hrotation}deg)`;
+minute.style.transform = `rotate(${mrotation}deg)`;
+second.style.transform = `rotate(${srotation}deg)`;
+ }, 1000);
+
+ 
